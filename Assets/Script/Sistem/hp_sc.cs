d2a83acb99e6023a0ce055b3e12f170641116881
paricann <hharu05@icloud.com>
2024-11-07 23:04:23 +0900

    paricann/playerの処理（キャラクターベースを使用） #9
    charactorbaseの処理を作成
    それに合わせてHpスクリプトの処理を修正
    テストはまだ済んでいない

--- Assets/Script/Sistem/hp_sc.cs
+++ Assets/Script/Sistem/hp_sc.cs
@@ -1,3 +1,4 @@
+using fighting_game;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,46 +6,56 @@ using UnityEngine.UI;
 
 public class hp_sc : MonoBehaviour
 {
-    public Enemy ene;
-    public Playercon _player;
+    public charactorbase _chbase;
     [SerializeField] public Slider slider;
-    [SerializeField] public float maxhelth;
     public float curenthelth = 0; 
     public float curentenehelth = 0;
 
     private void Start()
     {
         Debug.Log("ここまで来てるよ");
-        curenthelth = maxhelth;      //初期体力MAX
-        curentenehelth = maxhelth;   //上と同じ
+        curenthelth = _chbase.Maxhelth();      //初期体力MAX
+        curentenehelth = _chbase.Maxhelth();   //上と同じ
         UpdateenehelthUI();
         UpdateplayhelthUI();
     }
 
-    public void Hitplayerdamge(float dmg)
+    /// <summary> /// player体力減少処理 /// </summary>　
+    /// /// <param name="dmgs">_chbaseの_chpower</param>
+    public void Hitplayerdamge(float[] dmgs)
     {
         Debug.Log(curenthelth);
-        curenthelth -= dmg;
+        foreach( float dmg in dmgs)
+        {
+            curenthelth -= dmg;
+        }
         UpdateplayhelthUI();
-    }   
+    }
 
-    public void Hitenemydame(float dmg)
+    /// <summary>/// enemy体力減少処理/// </summary> ///
+    /// <param name="dmgs">_chbaseの_chpower </param>
+    public void Hitenemydame(float[] dmgs)
     {
         Debug.Log(curentenehelth);
-        curentenehelth -= dmg;
+        foreach (float dmg in dmgs)
+        {
+            curenthelth -= dmg;
+        }
         UpdateenehelthUI();
     }
     
+    /// <summary> /// playerのスライダー増減処理 /// </summary>
     public void UpdateplayhelthUI()
     {
         Debug.Log("playerhelth");
-        slider.value = curenthelth / maxhelth;
+        slider.value = curenthelth / _chbase.Maxhelth();
     }
 
+    /// <summary> /// enemyのスライダー増減処理 /// </summary>
     public void UpdateenehelthUI()
     {
         Debug.Log("enehelth");
-        slider.value = curentenehelth / maxhelth;
+        slider.value = curentenehelth / _chbase.Maxhelth();
     }
 
 }
